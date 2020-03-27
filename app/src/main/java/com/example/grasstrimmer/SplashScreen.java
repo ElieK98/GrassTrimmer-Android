5508274db527ec2ab7432e1e533961394a131f38
ElieK98 <elie.khal98@gmail.com>
2020-03-27 14:02:04 +0200

    latest commit

--- app/src/main/java/com/example/grasstrimmer/SplashScreen.java
+++ app/src/main/java/com/example/grasstrimmer/SplashScreen.java
@@ -12,6 +12,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen1);
        // startActivity(new Intent(this,MainActivity.class));
         Handler handler=new Handler();
 
